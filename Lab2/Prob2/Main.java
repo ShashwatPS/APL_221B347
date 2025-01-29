588ed34e0b106223400bf75f6011dcb81f699166
221B283 <221B283@juet.co.in>
2025-01-29 14:44:20 +0530

    Update Lab2 Prob1

---
+++ Lab2/Prob2/Main.java
@@ -0,0 +1,50 @@
+abstract class Voice2 {
+    // Template Method enforcing the correct sequence
+    public final void templateMethod() {
+        prepareVoice();
+        hear();
+    }
+
+    // Abstract methods to be implemented by subclasses
+    protected abstract void prepareVoice();
+    protected abstract void hear();
+}
+
+// Example subclass for Dog
+class DogVoice extends Voice2 {
+    @Override
+    protected void prepareVoice() {
+        System.out.println("Warming up the dog's voice...");
+    }
+
+    @Override
+    protected void hear() {
+        System.out.println("Dog barks: Woof! Woof!");
+    }
+}
+
+// Example subclass for Cat
+class CatVoice extends Voice2 {
+    @Override
+    protected void prepareVoice() {
+        System.out.println("Warming up the cat's voice...");
+    }
+
+    @Override
+    protected void hear() {
+        System.out.println("Cat meows: Meow! Meow!");
+    }
+}
+
+// Main class to test the implementation
+public class Main {
+    public static void main(String[] args) {
+        Voice2 dog = new DogVoice();
+        dog.templateMethod();  // Enforces correct order
+
+        System.out.println();
+
+        Voice2 cat = new CatVoice();
+        cat.templateMethod();  // Enforces correct order
+    }
+}
