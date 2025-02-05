c053b89517c559ddfbb5b5913eac10d959aba405
221B283 <221B283@juet.co.in>
2025-02-05 14:35:08 +0530

    Lab3 Added

---
+++ Lab3/Prob1/Arjun.java
@@ -0,0 +1,8 @@
+// Arjun.java
+public class Arjun extends Pandav {
+
+    @Override
+    public String kind() {
+        return "Arjun is very kind.";
+    }
+}
---
+++ Lab3/Prob1/Kaurav.java
@@ -0,0 +1,18 @@
+// Kaurav.java
+public class Kaurav extends Bharatvanshi {
+
+    @Override
+    public String fight() {
+        return "Kaurav fights fiercely.";
+    }
+
+    // Method to disobey orders
+    public String obey() {
+        return "Kaurav disobeys orders.";
+    }
+
+    // Method for cruelty (Kauravs are cruel)
+    public String kind() {
+        return "Kaurav is cruel.";
+    }
+}
